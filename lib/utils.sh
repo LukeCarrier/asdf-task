83bf76f5c5082d89dcd8761e18cebc32be43af3d
Luke Carrier <luke@carrier.im>
2023-01-21 12:26:45 +0000

    Support for Windows

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,6 +1,10 @@
 # get the OS family name
 get_os() {
-  uname | tr '[:upper:]' '[:lower:]'
+  if [[ "$(uname)" == MSYS_NT* ]]; then
+    echo windows
+  else
+    uname | tr '[:upper:]' '[:lower:]'
+  fi
 }
 
 # get the cpu architecture
@@ -20,6 +24,48 @@ get_arch() {
     *)
       # e.g. "arm64"
       echo $arch
+      ;;
+  esac
+}
+
+get_archive_fmt() {
+  local -r os=$1
+
+  case $os in
+    windows)
+      echo zip
+      ;;
+    *)
+      echo tar.gz
+      ;;
+  esac
+}
+
+extract_archive() {
+  local -r fmt=$1
+  local -r download_pkg=$2
+  local -r dest=$3
+  local -r tool=$4
+
+  case $fmt in
+    tar.gz)
+      tar xf "$download_pkg" -C "$dest" "$tool" 2>/dev/null
+      ;;
+    zip)
+      unzip -u "$download_pkg" "$tool" -d "$dest" 2>/dev/null
+      ;;
+  esac
+}
+
+get_bin_ext() {
+  local -r os=$1
+
+  case $os in
+    windows)
+      echo .exe
+      ;;
+    *)
+      echo
       ;;
   esac
 }
